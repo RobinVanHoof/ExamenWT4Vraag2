64302bf4b34720e53f34867ae14deb6d1cc5eacb
RobVH92 <robin_van_hoof@hotmail.com>
2017-09-07 17:01:54 +0200

    add db

--- recepten/models.py
+++ recepten/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class recept(models.Model):
+	recept_naam = models.CharField(max_length=50)
+	recept_calorie = models.CharField(max_length=500)
+    recept_ingredient = models.CharField(max_length=500)
+    recept_tijd = models.CharField(max_length=500)
+
+	def __unicode__(self):
+		return self.recept_naam
--- recepten/views.py
+++ recepten/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import django
 
 # Create your views here.
+
+
+def voegToe(request):
+    if request.method == 'POST':
+        naam = request.POST.get('naam')
+        cal = request.POST.get('cal')
+        ingr = request.POST.get('ingr')
+        tijd = request.POST.get('tijd)
+        if len(question) == 0:
+            return render(request, 'magiceightball/index.html', {'answer': 'You have to ask something'})
+
+        answer = r.get('question:' + question)
+        if answer == None:
+
+            answer = random.choice(possible_answers)
+            answerindex = possible_answers.index(answer)
+            while not cangiveanswer(answerindex):
+                answer = random.choice(possible_answers)
+                answerindex = possible_answers.index(answer)
+
+            r.incr('answer:' + str(answerindex))
+            r.set('question:' + question, answer)
+        return render(request, 'magiceightball/index.html', {'answer': answer} )
